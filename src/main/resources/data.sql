a584471273b556b5b4f7c99e272863a6e017955a
Walter Orellana Medi <walterorellanamedi@gmail.com>
2026-03-17 18:47:55 -0300

    fix: add dummy statement to data.sql to avoid empty script error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,5 @@
 
 -- Agregue aquí inserts reales si es necesario.
 -- Ejemplo: INSERT INTO personal (run, nombre, apellidos, cargo, email_institucional) VALUES ('12345678-9', 'JUAN', 'PEREZ', 'CARGO', 'email@colegioacropolis.cl');
+
+SELECT 1;
